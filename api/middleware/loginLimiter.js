972640de94d1eec04caa2fe92054d246443f8d65
Alen <alenaju1304@gmail.com>
2024-04-25 16:05:41 +0400

    added new limit to api endpoints

--- api/middleware/loginLimiter.js
+++ api/middleware/loginLimiter.js
@@ -2,7 +2,7 @@ import ratelimit from 'express-rate-limit';
 
 export const loginLimiter = ratelimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
-    max: 4, // limit each IP to 100 requests per windowMs
+    max: 100, // limit each IP to 100 requests per windowMs
     message: {message:'Too many requests from this IP, please try again after 15 minutes'},
     //sends a 429 status code
     handler: (req, res) => {
