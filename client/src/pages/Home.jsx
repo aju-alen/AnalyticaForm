165bc1847922c9798184ccf499a95644a42bb8e4
Alen <alenaju1304@gmail.com>
2024-04-26 11:49:21 +0400

    added test light mode

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,47 @@
 import HomeNavBar from '../components/HomeNavBar';
+import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Hero from '../components/Hero';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
 
 
 
 
 export default function Home() {
+
+  const [mode, setMode] = React.useState(localStorage.getItem('mode') === 'true' ? true : false);
+  const prefersDarkMode = useMediaQuery(`(prefers-color-scheme:${mode?"light":"dark"} )`);
+
+  const handleChange = (event) => {
+    setMode(event.target.checked);
+    localStorage.setItem('mode', event.target.checked);
+
+  };
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
   return (
+       <ThemeProvider theme={theme}>
     <div>
       <CssBaseline />
       <HomeNavBar />
+      <Switch
+      sx={{ position: 'fixed', top: 0, right: 0 }}
+      checked={mode}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
       <Hero />
     </div>
-
+    </ThemeProvider>
   );
 }
