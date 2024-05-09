0c88290bfd144ef27abac6ea154dde239d90e990
Alen <alenaju1304@gmail.com>
2024-05-09 10:57:27 +0400

    added excel export route

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import { corsOptions } from './utils/corsFe.js';
 import authRoute from './routes/auth-route.js';
 import surveyRoute from './routes/survey-route.js';
 import userResponseSurveyRoute from './routes/user-response-survey.route.js';
-import ExcelJS from 'exceljs';
+import excelRoute from './routes/excel-route.js';
 const app = express();
 
 app.use(cors(corsOptions)); 
@@ -17,30 +17,7 @@ app.use('/api/auth', authRoute)
 app.use('/api/survey', surveyRoute)
 app.use('/api/user-response-survey', userResponseSurveyRoute)
 
-app.post('/export-to-excel', async (req, res) => {
-    const data = req.body;
-
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Data');
-
-    // Add headers
-    worksheet.addRow(['Question', 'Answer']);
-
-    // Add data rows
-    data.userResponse.forEach(item => {
-        item.selectedValue.forEach(selected => {
-            worksheet.addRow([selected.question,selected.answer]);
-        });
-    });
-
-    // Write to buffer
-    const buffer = await workbook.xlsx.writeBuffer();
-
-    // Send buffer as response
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.setHeader('Content-Disposition', 'attachment; filename=data.xlsx');
-    res.send(buffer);
-});
+app.use('/api/excel',excelRoute)
 
 
 
