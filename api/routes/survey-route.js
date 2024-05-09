0c1e77cc5c5449012f263717d5590c7edc512564
Alen <alenaju1304@gmail.com>
2024-05-09 10:58:50 +0400

    added userresp for single survey route

--- api/routes/survey-route.js
+++ api/routes/survey-route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { createNewSurvey,getUserSurvey,getSurveyById,updateSurveyById} from '../controllers/survey-controller.js';
+import { createNewSurvey,getUserSurvey,getSurveyById,updateSurveyById,getAllSurveyResponse} from '../controllers/survey-controller.js';
 import { apiCallLimiter } from '../middleware/rateLimiter.js'
 import { verifyJwt } from '../middleware/verifyJwt.js';
 
@@ -8,6 +8,7 @@ router.post('/create',apiCallLimiter,verifyJwt, createNewSurvey);
 router.get('/get-all-survey',apiCallLimiter,verifyJwt, getUserSurvey); // get all survey of user
 router.get('/get-one-survey/:surveyId',apiCallLimiter,verifyJwt, getSurveyById); // get one survey data of the user
 router.put('/get-one-survey/:surveyId',apiCallLimiter,verifyJwt, updateSurveyById); // update one survey data of the user
+router.get('/get-all-user-response/:surveyId',apiCallLimiter,verifyJwt, getAllSurveyResponse); // Get all the user response of a particular survey
 
 
 export default router;
