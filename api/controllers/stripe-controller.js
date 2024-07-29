acc16a11f622adf275e6fd2c6c7ed66614c4e06d
Alen <alenaju1304@gmail.com>
2024-07-29 15:02:15 +0400

    stripe cancel redirect change

--- api/controllers/stripe-controller.js
+++ api/controllers/stripe-controller.js
@@ -29,7 +29,7 @@ export const createCheckoutSessionForSubscription = async (req, res) => {
             ],
             mode: 'subscription',
             success_url: `${YOUR_DOMAIN}/payment-success?success=true&session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: `${YOUR_DOMAIN}/dashboard`,
+            cancel_url: `${YOUR_DOMAIN}/product-display`,
           });
           console.log(session,'session');
         
