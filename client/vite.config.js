a0977891200947c1b22bffe9509d637d7d009080
Alen <alenaju1304@gmail.com>
2024-09-16 16:23:01 +0400

    added vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,18 @@ export default defineConfig({
   define: {
     global: {},
 },
+build: {
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes('node_modules')) {
+          // Create separate chunks for dependencies
+          return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        }
+      },
+    },
+  },
+  // Optionally adjust the chunk size warning limit
+  chunkSizeWarningLimit: 1000, // Adjust the limit (default is 500KB)
+},
 })
