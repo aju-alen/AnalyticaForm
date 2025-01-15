4238e430016c5f7443574472575c1719dd629f9f
aju-alen <alenaju1304@gmail.com>
2025-01-15 10:47:18 +0400

    added props fix

--- client/src/components/SEO.jsx
+++ client/src/components/SEO.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-export default function SEO({title, description, name, type,schema,surveyImage,surveyUrl}) {
+export default function SEO({title, description, name, type,schema,surveyImage,surveyUrl,robotText}) {
 return (
 <Helmet>
 { /* Standard metadata tags */ }
