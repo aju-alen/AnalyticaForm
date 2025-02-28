717191319063e507a29e65469f27f51325e49ac1
aju-alen <alenaju1304@gmail.com>
2025-02-28 09:11:31 +0400

    feat(vertex-ai-route): add verify middleware

--- api/routes/open-ai-route.js
+++ api/routes/open-ai-route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import { vertexChat } from '../controllers/open-ai-controller.js';
+import { verifyJwt } from '../middleware/verifyJwt.js';
 
-router.post('/chat',vertexChat);
+router.post('/chat',verifyJwt,vertexChat);
 
 export default router;
