f2655a6a0e4d69fdd54aa99fcb583160754465d7
aju-alen <alenaju1304@gmail.com>
2025-03-03 09:55:07 +0400

    refactor(dynamic-html-route): change description content

--- api/controllers/dynamic-html-preview-controller.js
+++ api/controllers/dynamic-html-preview-controller.js
@@ -24,13 +24,15 @@ export const dynamicMetaHtml = async (req, res) => {
             <head>
                 <title>Dubai Analytica: ${survey.surveyTitle}</title>
                 <meta name="robots" content="index, follow" />
-                <meta name="description" content="${survey.surveyDescription}" />
+                <meta name="description" content="All-in-One Survey Software for Market and Academic Research
+                Create, distribute, and analyse surveys—all in one place. Gain deeper insights with data gathered from surveys." />
                 
                 <!-- Facebook Open Graph Tags -->
                 <meta property="og:site_name" content="Dubai Analytica" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="${survey.surveyTitle}" />
-                <meta property="og:description" content="${survey.surveyDescription}" />
+                <meta property="og:description" content="All-in-One Survey Software for Market and Academic Research
+                Create, distribute, and analyse surveys—all in one place. Gain deeper insights with data gathered from surveys." />
                 <meta property="og:image" content="https://dubai-analytica.s3.ap-south-1.amazonaws.com/image/NavbarLogo.png" />
                 <meta property="og:url" content="https://dubaianalytica.com/user-survey/${surveyId}" />
                 
@@ -38,7 +40,8 @@ export const dynamicMetaHtml = async (req, res) => {
                 <meta name="twitter:creator" content="Dubai Analytica" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="Survey Title" />
-                <meta name="twitter:description" content="${survey.surveyDescription}" />
+                <meta name="twitter:description" content="All-in-One Survey Software for Market and Academic Research
+                Create, distribute, and analyse surveys—all in one place. Gain deeper insights with data gathered from surveys." />
                 <meta name="twitter:image" content="https://dubai-analytica.s3.ap-south-1.amazonaws.com/image/NavbarLogo.png" />
                 <meta http-equiv="refresh" content="0;url=https://dubaianalytica.com/user-survey/${surveyId}" />
             </head>
