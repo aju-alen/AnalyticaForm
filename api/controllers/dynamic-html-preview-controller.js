0238b77b5ae5c2c192ff19acd62cbe32ce68fc8a
aju-alen <alenaju1304@gmail.com>
2025-03-03 09:43:43 +0400

    refactor(dynamic-html-tag): change html tags

--- api/controllers/dynamic-html-preview-controller.js
+++ api/controllers/dynamic-html-preview-controller.js
@@ -22,10 +22,24 @@ export const dynamicMetaHtml = async (req, res) => {
             <!DOCTYPE html>
             <html lang="en">
             <head>
+                <title>Dubai Analytica: ${survey.surveyTitle}</title>
+                <meta name="robots" content="index, follow" />
+                <meta name="description" content="${survey.surveyDescription}" />
+                
+                <!-- Facebook Open Graph Tags -->
+                <meta property="og:site_name" content="Dubai Analytica" />
+                <meta property="og:type" content="website" />
                 <meta property="og:title" content="${survey.surveyTitle}" />
-                <meta property="og:image" content="${survey.image || 'https://dubai-analytica.s3.ap-south-1.amazonaws.com/image/NavbarLogo.png'}" />
+                <meta property="og:description" content="${survey.surveyDescription}" />
+                <meta property="og:image" content="https://dubai-analytica.s3.ap-south-1.amazonaws.com/image/NavbarLogo.png" />
                 <meta property="og:url" content="https://dubaianalytica.com/user-survey/${surveyId}" />
+                
+                <!-- Twitter Tags -->
+                <meta name="twitter:creator" content="Dubai Analytica" />
                 <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Survey Title" />
+                <meta name="twitter:description" content="${survey.surveyDescription}" />
+                <meta name="twitter:image" content="https://dubai-analytica.s3.ap-south-1.amazonaws.com/image/NavbarLogo.png" />
                 <meta http-equiv="refresh" content="0;url=https://dubaianalytica.com/user-survey/${surveyId}" />
             </head>
             <body></body>
