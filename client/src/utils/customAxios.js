97b68955c19afe347126265f6c4fc4cc4ba9b90b
aju-alen <alenaju1304@gmail.com>
2025-03-03 12:00:48 +0400

    feat(custom-axios): add header tags

--- client/src/utils/customAxios.js
+++ client/src/utils/customAxios.js
@@ -1,16 +1,38 @@
 import axios from 'axios';
 
 // Custom Axios instance with no headers or credentials
-export const axiosNoHeaders = axios.create();
+export const axiosNoHeaders = axios.create({
+  headers: {
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+    'Content-Security-Policy': "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval'; img-src 'self' https: data:; font-src 'self' https: data:;",
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+  },
+});
 
 // Custom Axios instance with credentials enabled
 export const axiosWithCredentials = axios.create({
   withCredentials: true,
+  headers: {
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+    'Content-Security-Policy': "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval'; img-src 'self' https: data:; font-src 'self' https: data:;",
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+  },
 });
 
 // Custom Axios instance with Authorization header and credentials enabled
 export const axiosWithAuth = axios.create({
   withCredentials: true,
+  headers: {
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+    'Content-Security-Policy': "default-src 'self' https: data: 'unsafe-inline' 'unsafe-eval'; img-src 'self' https: data:; font-src 'self' https: data:;",
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+  },
 });
 
 // Add a request interceptor to attach the Authorization header
@@ -23,4 +45,4 @@ axiosWithAuth.interceptors.request.use(config => {
   return config;
 });
 
-// Usage: Some api needs credentials(Bearer and cookies), some don't. This is where customAxios comes in handy.
+// Usage: Some API needs credentials (Bearer and cookies), some don't. This is where customAxios comes in handy.
