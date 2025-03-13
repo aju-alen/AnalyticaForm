98d38cf7ddbf4bf6c5623ea17b0422f61ad410e4
aju-alen <alenaju1304@gmail.com>
2025-03-13 09:13:37 +0400

    feat(test-response): url redirect

--- client/src/pages/TestPage.jsx
+++ client/src/pages/TestPage.jsx
@@ -4,13 +4,9 @@ const TestPage = () => {
   useEffect(() => {
     // Make the GET request to the backend API
     fetch(`https://backend.dubaianalytica.com/survey-meta/cm82wzqdc000379zm5rj1g29c`)
-      .then((response) => response.json())
-      .then((data) => {
-        // You can perform any necessary actions with the data here
-        console.log(data);
-
-        // Redirect to another URL after the API call
-        window.location.href = `https://dubaianalytica.com/user-survey/cm82wzqdc000379zm5rj1g29c`;
+      .then((response) => {
+        // The backend will handle the redirect, so we don't need to process the response
+        console.log('Request sent to backend');
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
