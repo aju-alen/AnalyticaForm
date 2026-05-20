ac8983b89c48b7ef308f253afdb293e56706f335
aju-alen <alenaju1304@gmail.com>
2026-05-20 14:17:10 +0400

    route name fix

--- api/routes/mtcm-route.js
+++ api/routes/mtcm-route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { ContactUs, MembershipEnquiry } from '../controllers/resendEmailController.js';
+import { ContactUs, MembershipEnquiry,ResourcesRequest } from '../controllers/resendEmailController.js';
 
 router.post('/contact-us', ContactUs);
 router.post('/membership-enquiry', MembershipEnquiry);
